c487e2906b47f75725d828fd6064268c885ac3a3
AlexisFil <dydimos88@gmail.com>
2020-06-03 16:16:24 +0300

    test

--- single_value_list.py
+++ single_value_list.py
@@ -3,7 +3,7 @@ myList = []
 while True:
     choice = input('''Menu: \n-1.new entry \n-2. show \n-3.exit \nSelect >''')
     if choice !='1' and choice !='2' and choice != '3':                                               #Restart if not menu choice
-                   print("Invalid choice please select a command from the menu\n")
+                   print("Invalid choice please select a command from  menu\n")
                    continue
                 
     if choice =='1':                                                                                  
